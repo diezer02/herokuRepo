b6a40acb80dc8baa6bdf5dfb0e9dd507d512d990
diezer02 <diezerjeff@gmail.com>
2017-06-20 10:35:15 +0200

    modify

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,8 +16,11 @@
 
 package com.example;
 
+import static javax.measure.unit.SI.KILOGRAM;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.jscience.physics.amount.Amount;
+import org.jscience.physics.model.RelativisticModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -26,6 +29,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.measure.quantity.Mass;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -84,5 +88,15 @@ public class Main {
       return new HikariDataSource(config);
     }
   }
-
+  @RequestMapping("/hello")
+  String hello(Map<String, Object> model) {
+    RelativisticModel.select();
+    String energy = System.getenv().get("ENERGY");
+    if (energy == null) {
+      energy = "12 GeV";
+    }
+    Amount<Mass> m = Amount.valueOf(energy).to(KILOGRAM);
+    model.put("science", "E=mc^2: " + energy + " = "  + m.toString());
+    return "hello";
+  }
 }
